72d02e0bee9d40fade03fff8d2be56d460c72788
Cameron Gagnon <cameron.gagnon@gmail.com>
2017-10-31 21:56:47 -0400

    Add test_rpn.py and fixup rpn.py

--- rpn.py
+++ rpn.py
@@ -1,1 +1,36 @@
-rpn_basic.py
+#!/usr/bin/env python3
+
+import operator
+
+
+operators = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.truediv,
+}
+
+def calculate(myarg):
+    stack = list()
+    for token in myarg.split():
+        try:
+            token = int(token)
+            stack.append(token)
+        except ValueError:
+            function = operators[token]
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = function(arg1, arg2)
+            stack.append(result)
+        print(stack)
+    if len(stack) != 1:
+        raise TypeError("Too many parameters")
+    return stack.pop()
+
+def main():
+    while True:
+        result = calculate(input("rpn calc> "))
+        print("Result: ", result)
+
+if __name__ == '__main__':
+    main()
